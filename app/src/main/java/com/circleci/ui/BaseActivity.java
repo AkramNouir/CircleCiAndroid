606fa3742ace14df2036d9783a53cde600e753ad
akramnouir <akram.nouir@ifsalpha.com>
2018-01-29 11:51:21 +0100

    Add Ui structure

---
+++ app/src/main/java/com/circleci/ui/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.circleci.ui;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by akramnouir on 29/01/2018.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/circleci/ui/BaseFragment.java
@@ -0,0 +1,10 @@
+package com.circleci.ui;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by akramnouir on 29/01/2018.
+ */
+
+public class BaseFragment extends Fragment {
+}
